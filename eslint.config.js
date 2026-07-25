6eb26d8e4e73b6534f999664529c1541bf000901
Sanket Sayal <snktsayal@gmail.com>
2026-07-25 11:34:32 +0530

    prittier setup

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
   {
@@ -69,4 +70,5 @@ export default tseslint.config(
       curly: ["error", "all"],
     },
   },
+  prettier
 );
