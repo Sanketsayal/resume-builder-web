20622406bd247f3342c6ca22f1c09cdfd8d24409
Sanket Sayal <snktsayal@gmail.com>
2026-07-28 22:52:55 +0530

    router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+
+import { router } from "./app/router";
+import "@/styles/index.css";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import { BrowserRouter } from "react-router-dom";
+import { router } from "./app/router";
+import "@/styles/index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./app/app";
+import { router } from "./app/router";
+import "@/styles/index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import "./styles/index.css";
+import { router } from "./app/router";
+import "@/styles/index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
