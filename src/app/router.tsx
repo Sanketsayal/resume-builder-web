0ca5cafd89d1a727093d6301a2b28d7f2e7c6d26
Sanket Sayal <snktsayal@gmail.com>
2026-08-04 21:53:33 +0530

    fix

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,3 @@
-import AuthLayout from "../layouts/AuthLayout";
-import DashboardLayout from "../layouts/DashboardLayout";
-import RootLayout from "../layouts/RootLayout";
-import { ForgotPasswordPage } from "../pages/auth/ForgotPasswordPage";
-import { LoginPage } from "../pages/auth/LoginPage";
-import { RegisterPage } from "../pages/auth/RegisterPage";
-import DashboardPage from "../pages/dashboard/DashboardPage";
-import { NotFoundPage } from "../pages/errors/NotFoundPage";
-import { ProfilePage } from "../pages/profile/ProfilePage";
-import { ResumeCreatePage } from "../pages/resume/ResumeCreatePage";
-import { ResumeEditorPage } from "../pages/resume/ResumeEditorPage";
-import { ResumeListPage } from "../pages/resume/ResumeListPage";
-import { ResumePreviewPage } from "../pages/resume/ResumePreviewPage";
 import { createBrowserRouter } from "react-router-dom";
 
 import AuthLayout from "../layouts/AuthLayout";
@@ -26,37 +13,6 @@ import { ResumeCreatePage } from "../pages/resume/ResumeCreatePage";
 import { ResumeEditorPage } from "../pages/resume/ResumeEditorPage";
 import { ResumeListPage } from "../pages/resume/ResumeListPage";
 import { ResumePreviewPage } from "../pages/resume/ResumePreviewPage";
-import { createBrowserRouter } from "react-router-dom";
-
-import AuthLayout from "../layouts/AuthLayout";
-import DashboardLayout from "../layouts/DashboardLayout";
-import RootLayout from "../layouts/RootLayout";
-import { ForgotPasswordPage } from "../pages/auth/ForgotPasswordPage";
-import { LoginPage } from "../pages/auth/LoginPage";
-import { RegisterPage } from "../pages/auth/RegisterPage";
-import DashboardPage from "../pages/dashboard/DashboardPage";
-import { NotFoundPage } from "../pages/errors/NotFoundPage";
-import { ProfilePage } from "../pages/profile/ProfilePage";
-import { ResumeCreatePage } from "../pages/resume/ResumeCreatePage";
-import { ResumeEditorPage } from "../pages/resume/ResumeEditorPage";
-import { ResumeListPage } from "../pages/resume/ResumeListPage";
-import { ResumePreviewPage } from "../pages/resume/ResumePreviewPage";
-import { createBrowserRouter } from "react-router-dom";
-
-import AuthLayout from "../layouts/AuthLayout";
-import DashboardLayout from "../layouts/DashboardLayout";
-import RootLayout from "../layouts/RootLayout";
-import { ForgotPasswordPage } from "../pages/auth/ForgotPasswordPage";
-import { LoginPage } from "../pages/auth/LoginPage";
-import { RegisterPage } from "../pages/auth/RegisterPage";
-import DashboardPage from "../pages/dashboard/DashboardPage";
-import { NotFoundPage } from "../pages/errors/NotFoundPage";
-import { ProfilePage } from "../pages/profile/ProfilePage";
-import { ResumeCreatePage } from "../pages/resume/ResumeCreatePage";
-import { ResumeEditorPage } from "../pages/resume/ResumeEditorPage";
-import { ResumeListPage } from "../pages/resume/ResumeListPage";
-import { ResumePreviewPage } from "../pages/resume/ResumePreviewPage";
-import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
