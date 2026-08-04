75cfe8858e4c7045f17e81c4bad8211f20282748
Sanket Sayal <snktsayal@gmail.com>
2026-08-04 21:52:12 +0530

    fix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,8 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { PropsWithChildren } from "react";
 
 import { queryClient } from "../lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-import { queryClient } from "../lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-import { queryClient } from "../lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-import { queryClient } from "../lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
